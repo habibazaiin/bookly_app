af489615df4b96b9ea981e336e739cb9c2af7bb3
habibazaiin <zain.habiba0@gmail.com>
2025-10-02 13:23:41 +0300

    add custom app bar to home view

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,4 +2,4 @@ import 'dart:ui';
 
 const kPrimaryColor = Color(0XFF100B20);
 
-const ktransitionDuration = Duration(microseconds: 250);
+const ktransitionDuration = Duration(seconds: 2);
---
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -0,0 +1,33 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [CustomAppBar()]);
+  }
+}
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(Assets.logo, height: 20),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(FontAwesomeIcons.magnifyingGlass, size: 22),
+          ),
+        ],
+      ),
+    );
+  }
+}
