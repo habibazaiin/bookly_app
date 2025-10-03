cb664b7e3c56a2c6dc6aa74e163c1da260aa562a
habibazaiin <zain.habiba0@gmail.com>
2025-10-03 15:28:56 +0300

    add featured list view to home view

---
+++ lib/features/home/presentation/view/widgets/featured_list_view_item.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 2.5 / 4,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: Colors.red,
+          image: DecorationImage(
+            image: NetworkImage(
+              'https://th.bing.com/th/id/OSK.56824888c78d672aece69be7be5d66a2?w=160&h=238&c=7&rs=1&qlt=80&o=6&dpr=1.3&pid=SANGAM',
+            ),
+            fit: BoxFit.fill,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/view/widgets/home_view_body.dart
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -1,5 +1,5 @@
 import 'package:bookly_app/core/utils/assets.dart';
-import 'package:bookly_app/features/home/presentation/view/widgets/custom_list_view_item.dart';
+import 'package:bookly_app/features/home/presentation/view/widgets/featured_list_view_item.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:bookly_app/features/home/presentation/view/widgets/custom_appbar.dart';
@@ -9,6 +9,26 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [CustomAppBar(), CustomListViewItem()]);
+    return Column(children: [CustomAppBar(), FeaturedListView()]);
+  }
+}
+
+class FeaturedListView extends StatelessWidget {
+  const FeaturedListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: FeaturedListViewItem(),
+          );
+        },
+      ),
+    );
   }
 }
