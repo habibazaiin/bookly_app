5d805812463ead690d7bab6f6e5bf109194d39be
habibazaiin <zain.habiba0@gmail.com>
2025-10-04 18:16:40 +0300

    create books button action

--- lib/features/home/presentation/view/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/view/widgets/book_details_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/core/widgets/custom_button.dart';
 import 'package:bookly_app/features/home/presentation/view/widgets/book_rating.dart';
+import 'package:bookly_app/features/home/presentation/view/widgets/books_action.dart';
 import 'package:bookly_app/features/home/presentation/view/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/view/widgets/featured_list_view_item.dart';
 import 'package:bookly_app/features/home/presentation/view/widgets/features_list_view.dart';
@@ -39,8 +41,11 @@ class BookDetailsViewBody extends StatelessWidget {
           ),
           SizedBox(height: 18),
           BookRating(mainAxisAlignment: MainAxisAlignment.center),
+          SizedBox(height: 37),
+          BooksAction(),
         ],
       ),
     );
   }
 }
+
---
+++ lib/features/home/presentation/view/widgets/books_action.dart
@@ -0,0 +1,40 @@
+import 'package:bookly_app/core/widgets/custom_button.dart';
+import 'package:flutter/material.dart';
+
+class BooksAction extends StatelessWidget {
+  const BooksAction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: CustomButton(
+              backgroundColor: Colors.white,
+              title: r'19.19$',
+              textColor: Colors.black,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(16),
+                bottomLeft: Radius.circular(16),
+              ),
+            ),
+          ),
+          Expanded(
+            child: CustomButton(
+              backgroundColor: Color(0XFFEF8262),
+              title: 'Free Preview',
+              textColor: Colors.white,
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(16),
+                bottomRight: Radius.circular(16),
+              ),
+              fontSize: 16,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
