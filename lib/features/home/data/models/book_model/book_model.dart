f3c01883e76cd1c6cf3ef766cd5abe40e5b3a695
habibazaiin <zain.habiba0@gmail.com>
2025-10-06 16:59:49 +0300

    create api service

--- lib/features/home/data/models/book_model/book_model.dart
+++ lib/features/home/data/models/book_model/book_model.dart
@@ -1,60 +1,28 @@
 import 'package:equatable/equatable.dart';
 
-import 'doc.dart';
+import 'item.dart';
 
 class BookModel extends Equatable {
-  final int? numFound;
-  final int? start;
-  final bool? numFoundExact;
-  final String? documentationUrl;
-  final String? q;
-  final dynamic offset;
-  final List<Doc>? docs;
+  final String? kind;
+  final int? totalItems;
+  final List<Item>? items;
 
-  const BookModel({
-    this.numFound,
-    this.start,
-    this.numFoundExact,
-    this.documentationUrl,
-    this.q,
-    this.offset,
-    this.docs,
-  });
+  const BookModel({this.kind, this.totalItems, this.items});
 
   factory BookModel.fromJson(Map<String, dynamic> json) => BookModel(
-    numFound: json['numFound'] as int?,
-    start: json['start'] as int?,
-    numFoundExact: json['numFoundExact'] as bool?,
-    documentationUrl: json['documentation_url'] as String?,
-    q: json['q'] as String?,
-    offset: json['offset'] as dynamic,
-    docs: (json['docs'] as List<dynamic>?)
-        ?.map((e) => Doc.fromJson(e as Map<String, dynamic>))
+    kind: json['kind'] as String?,
+    totalItems: json['totalItems'] as int?,
+    items: (json['items'] as List<dynamic>?)
+        ?.map((e) => Item.fromJson(e as Map<String, dynamic>))
         .toList(),
   );
 
   Map<String, dynamic> toJson() => {
-    'numFound': numFound,
-    'start': start,
-    'numFoundExact': numFoundExact,
-    'num_found': numFound,
-    'documentation_url': documentationUrl,
-    'q': q,
-    'offset': offset,
-    'docs': docs?.map((e) => e.toJson()).toList(),
+    'kind': kind,
+    'totalItems': totalItems,
+    'items': items?.map((e) => e.toJson()).toList(),
   };
 
   @override
-  List<Object?> get props {
-    return [
-      numFound,
-      start,
-      numFoundExact,
-      numFound,
-      documentationUrl,
-      q,
-      offset,
-      docs,
-    ];
-  }
+  List<Object?> get props => [kind, totalItems, items];
 }
