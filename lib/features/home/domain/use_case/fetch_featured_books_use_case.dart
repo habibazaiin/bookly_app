7197b0c07c931c394872428339714ea91d42d1c9
habibazaiin <zain.habiba0@gmail.com>
2025-10-14 17:25:18 +0300

    create fetch featured books use case

---
+++ lib/features/home/domain/use_case/fetch_featured_books_use_case.dart
@@ -0,0 +1,15 @@
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/core/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/domain/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchFeaturedBooksUseCase {
+  final HomeRepo homeRepo;
+
+  FetchFeaturedBooksUseCase({required this.homeRepo});
+
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() {
+    //check permission
+    return homeRepo.fetchFeaturedBooks();
+  }
+}
