e3823c73d1d8f24aeb82446e5e69d3a842ae61ac
habibazaiin <zain.habiba0@gmail.com>
2025-10-15 18:35:01 +0300

    implement fetch featured books local data source

--- lib/features/home/data/data_source/home_local_data_source.dart
+++ lib/features/home/data/data_source/home_local_data_source.dart
@@ -1,4 +1,6 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
+import 'package:hive_flutter/adapters.dart';
 
 abstract class HomeLocalDataSource {
   List<BookEntity> fetchNewestBooks();
@@ -9,8 +11,8 @@ abstract class HomeLocalDataSource {
 class HomeLocalDataSourceImpl extends HomeLocalDataSource {
   @override
   List<BookEntity> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+    var box = Hive.box<BookEntity>(kFeaturedBox);
+    return box.values.toList();
   }
 
   @override
--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 
 void main() async {
   setupServiceLocator();
   runApp(const BooklyApp());
+
+  await Hive.initFlutter();
   Hive.registerAdapter(BookEntityAdapter());
   await Hive.openBox(kFeaturedBox);
 }
