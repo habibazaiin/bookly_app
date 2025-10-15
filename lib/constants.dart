f1688d69e7b3f3af8efa645c7a6a51498b1bba16
habibazaiin <zain.habiba0@gmail.com>
2025-10-15 18:09:00 +0300

    register book entity adapter & open featured box

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,3 +5,5 @@ const kPrimaryColor = Color(0XFF100B20);
 const kTransitionDuration = Duration(seconds: 2);
 
 const kGtSectraFine = 'GT Sectra Fine';
+
+const kFeaturedBox = 'featured_box';
--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,19 @@ import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/service_locator.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
 import 'package:bookly_app/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
 
-void main() {
+void main() async {
   setupServiceLocator();
   runApp(const BooklyApp());
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox(kFeaturedBox);
 }
 
 class BooklyApp extends StatelessWidget {
