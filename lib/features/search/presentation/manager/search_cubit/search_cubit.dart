ec0a9b40d4cb9ff82326203579d54f415cc16a7a
habibazaiin <zain.habiba0@gmail.com>
2025-10-27 15:06:47 +0300

    refactor search cubit

--- lib/features/search/presentation/manager/search_cubit/search_cubit.dart
+++ lib/features/search/presentation/manager/search_cubit/search_cubit.dart
@@ -2,25 +2,18 @@ import 'package:bloc/bloc.dart';
 import 'package:bookly_app/core/errors/failure.dart';
 import 'package:bookly_app/core/models/book_model/book_model.dart';
 import 'package:bookly_app/features/home/domain/repos/home_repo.dart';
-import 'package:bookly_app/features/search/domain/search_repo.dart';
+
 import 'package:equatable/equatable.dart';
 
 part 'search_state.dart';
 
 class SearchCubit extends Cubit<SearchState> {
-  SearchCubit(this.searchRepo) : super(SearchInitial());
-  final SearchRepo searchRepo;
+  SearchCubit() : super(SearchInitial());
+
 
   Future<void> getResultSearch({required String search}) async {
     emit(SearchLoading());
-    var result = await searchRepo.fetchResultSearch(search: search);
-    result.fold(
-      (failure) {
-        emit(SearchFailure(errMessage: failure.errMessage));
-      },
-      (books) {
-        emit(SearchSuccess(books: books));
-      },
-    );
+    
+    
   }
 }
