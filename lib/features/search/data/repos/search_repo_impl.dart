5b9bc46016835ccd2abdae794aa6c90894802c14
habibazaiin <zain.habiba0@gmail.com>
2025-10-27 14:37:21 +0300

    refactor search repo implementation

--- lib/features/search/data/repos/search_repo_impl.dart
+++ lib/features/search/data/repos/search_repo_impl.dart
@@ -1,27 +1,32 @@
+import 'package:bookly_app/core/entities/book_entity.dart';
 import 'package:bookly_app/core/errors/failure.dart';
 import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:bookly_app/core/models/book_model/book_model.dart';
+import 'package:bookly_app/features/search/data/data_source/search_local_data_sourch.dart';
+import 'package:bookly_app/features/search/data/data_source/search_remote_data_source.dart';
 import 'package:bookly_app/features/search/domain/repos/search_repo.dart';
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 
-class SearchRepoImpl implements SearchRepo {
-  final ApiService apiService;
+class SearchRepoImpl extends SearchRepo {
+  final SearchLocalDataSource searchLocalDataSource;
+  final SearchRemoteDataSource searchRemoteDataSource;
 
-  SearchRepoImpl({required this.apiService});
+  SearchRepoImpl({
+    required this.searchLocalDataSource,
+    required this.searchRemoteDataSource,
+  });
   @override
-  Future<Either<Failure, List<BookModel>>> fetchResultSearch({required String search}) async {
+  Future<Either<Failure, List<BookEntity>>> fetchResultSearch({
+    required String search,
+  }) async {
     try {
-      var data = await apiService.get(
-        endPoint:
-            'volumes?Filtering=free-ebooks&Sorting=newest &q=$search',
-      );
-
-      List<BookModel> books = [];
-
-      for (var item in data['items']) {
-        books.add(BookModel.fromJson(item));
+      List<BookEntity> books;
+      books = searchLocalDataSource.fetchResultSearch(search: search);
+      if (books.isNotEmpty) {
+        return right(books);
       }
+      books = await searchRemoteDataSource.fetchResultSearch(search: search);
       return right(books);
     } on Exception catch (e) {
       if (e is DioException) {
--- lib/features/search/domain/repos/search_repo.dart
+++ lib/features/search/domain/repos/search_repo.dart
@@ -1,7 +1,7 @@
+import 'package:bookly_app/core/entities/book_entity.dart';
 import 'package:bookly_app/core/errors/failure.dart';
-import 'package:bookly_app/core/models/book_model/book_model.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class SearchRepo {
-  Future<Either<Failure, List<BookModel>>> fetchResultSearch({required String search});
+  Future<Either<Failure, List<BookEntity>>> fetchResultSearch({required String search});
 }
